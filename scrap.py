cbc8479326c6ac0cda6b89221305eabfe82add5c
sandeep <sandeepanandanpullarkattu@gmail.com>
2022-05-27 20:25:53 +0530

    first commit

--- scrap.py
+++ scrap.py
@@ -9,14 +9,6 @@ url = "https://www.hcpcsdata.com/Codes"
 reqs = requests.get(url)
 page1 = BeautifulSoup(reqs.content, "html.parser")
 
-
-# tr = soup.find_all('a')
-# print(tr)
-#
-# urls = []
-# for link in tr:
-#     print(link)
-#     print(link.get('href'))
 table_data = page1.find_all('td')
 table_row = page1.findAll('tr',class_="clickable-row")
 with open ('equipo.csv','w',encoding='utf8',newline='') as f:
@@ -52,32 +44,4 @@ with open ('equipo.csv','w',encoding='utf8',newline='') as f:
                 thewriter.writerow(info)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-    #
-#     for td in table_data:
-#         if td.find('a'):
-#             a = td.find('a').get('href')
-#             url2 = urllib.parse.urljoin(url, a)
-#             reqs2 = requests.get(url2)
-#             page2 = BeautifulSoup(reqs2.content, "html.parser")
-#             table_row2 = page2.findAll('tr', class_="clickable-row")
-#         for t_data in table_row2:
-#             code = t_data.find('a').text
-#             table = t_data.find_all('td')
-#
-#             if table[1]:
-#                 description = table[1].text.strip()
-# info2 = [group,category,code,description]
-            # thewriter.writerow(info2)
 
